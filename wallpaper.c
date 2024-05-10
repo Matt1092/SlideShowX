aae49f697d8321ff756fdf582f7b818308018cd8
mattdaboi <146141224+mattdaboi@users.noreply.github.com>
2024-05-10 18:07:26 -0400

    Update wallpaper.c

--- wallpaper.c
+++ wallpaper.c
@@ -10,10 +10,20 @@
 
 
 // Main function
-int main(void) {
+int main(void)
+{
+    // Declare a character array with maximum size of up to 256 characters
+    char foldername[256];
 
+    printf("C code application to spice up your desktop wallpapers\n");
+    printf("Folder name: ");
+
+    // Prompt user for folder name
+    scanf_s("%s", foldername, sizeof(foldername));
+    
     // Pointer to a wide character string
-    const wchar_t *file = L"C:\\Users\\mattm\\OneDrive\\Desktop\\whitecolour.png"; // Change this to the path of your image file
+//    const wchar_t *file = L"C:\\Users\\mattm\\OneDrive\\Desktop\\whitecolour.png"; // Change this to the path of your image file
+    const wchar_t* file = L"C:\\Users\\mattm\\OneDrive\\Desktop\\forest.png"; // Change this to the path of your image file
 
     // Windows API function used to set system parameters. Sets desktop wallpaper
     // Integer value storing return value of SystemParametersInfoW function call
